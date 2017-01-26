470d9cc800d8722476b34183ad30753ecc318606
Jieun Lee <jieunlee28@gmail.com>
2017-01-25 23:34:07 -0800

    Update preview image on selection

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // PHOTO VIEWER
 // by Jieun Lee
+
+$(document).ready(function(){
+
+	// action when preview image is clicked
+	$(".preview img").click(function() {
+		var imgSrc = $(this).attr("src");
+		$("#main img").attr("src", imgSrc);
+	});
+
+});
